1a959db61259263889e7883da2fa5317b8127c36
Alexander Ahlbrandt <alexander.ahlbrandt@aa.com>
2021-09-24 14:04:41 -0500

    1.2. bug fixes

--- changelog.md
+++ changelog.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 1.2 - 2021-09-24:
+ - Fix some bugs
+
 ## 1.1 - 2021-09-24:
  - Remove company specific urls
  - Add commander to help users supply details
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,27 @@ import Logger from 'jet-logger';
 import * as childProcess from 'child_process';
 import { program, Argument } from 'commander';
 
-program.version('1.1');
+program.version('1.2');
 program.addArgument(new Argument('sourceUrl', 'url of the enterprise git server Ex. ghe.yourCompany.com'))
 program.addArgument(new Argument('targetUrl', 'destination to migrate to. Ex. github.com'))
 program.addArgument(new Argument('sourceOrg', 'org to migrate from. Ex. ***REMOVED***'))
 program.addArgument(new Argument('targetOrg', 'org to migrate to. Ex. ***REMOVED***'))
-program.option('-d, --workdir <string>', 'target url to migrate to', '../')
+program.option('-d, --workdir <string>', 'target url to migrate to', '..')
 program.parse();
 const logger = new Logger();
+
+const sleep = async (timeout: number) => {
+  await new Promise((res) => {
+    setTimeout(res, timeout);
+  })
+}
+
 function exec(cmd: string, loc: string): Promise<string> {
   return new Promise((res, rej) => {
     return childProcess.exec(cmd, { cwd: loc }, (err, stdout, stderr) => {
+      if (stderr.includes('not a git repository') || stderr.includes('No such remote')) {
+        return res('skipped');
+      }
       if (!!stdout) {
         // logger.info(stdout);
       }
@@ -25,26 +35,27 @@ function exec(cmd: string, loc: string): Promise<string> {
 }
 
 const main = async (projectRoot: string, sourceUrl: string, targetUrl: string, sourceOrg: string, targetOrg: string) => {
+  let migrated = 0;
+  await sleep(3000);
   const dirRes = await exec('ls -d */', projectRoot);
   const dirs = dirRes.trim().split('\n');
-  dirs.forEach(async dir => {
-    try {
-      let origin = await exec(`git remote get-url origin`, `${projectRoot}/${dir}`);
-      if (!origin.includes(sourceUrl)) {
-        logger.info(`skipping directory ${dir}`);
-        return;
-      }
-      origin = origin.replace(`${sourceUrl}:${sourceOrg}`, `${targetUrl}:${targetOrg}`);
-      await exec(`git remote set-url origin ${origin}`, `${projectRoot}/${dir}`)
-      logger.info(`migrated ${dir}!`);
-    } catch (e) {
-      if (e.toString().includes('not a git repository')) {
-        return;
-      }
-      throw e;
+  for (const dir of dirs) {
+    let origin = await exec(`git remote get-url origin`, `${projectRoot}/${dir}`);
+    if (!origin.includes(`${sourceUrl}:${sourceOrg}`)) {
+      logger.info(`skipping directory ${dir}`);
+      continue;
     }
-  });
+    origin = origin.replace(`${sourceUrl}:${sourceOrg}`, `${targetUrl}:${targetOrg}`);
+    const result = await exec(`git remote set-url origin ${origin}`, `${projectRoot}/${dir}`);
+    if (result !== 'skipped') {
+      logger.info(`migrated ${dir}!`);
+      migrated++;
+      continue;
+    }
+  }
+  logger.info(`Migrated ${migrated} repo(s). Exiting.`)
 }
 
-const { workdir, sourceUrl, targetUrl, sourceOrg, targetOrg } = program.opts();
-main(workdir, sourceUrl, targetUrl, sourceOrg, targetOrg);
+const { workdir } = program.opts();
+const [sourceUrl, targetUrl, sourceOrg, targetOrg] = program.args;
+main(workdir, sourceUrl, targetUrl, sourceOrg, targetOrg).catch(x => console.log('x', x));
